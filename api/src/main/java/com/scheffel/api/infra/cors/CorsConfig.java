923ff73a361cd6828f17c7c8057a8e65be562d4e
muriloscheffel <murilo.scheffel@outlook.com>
2025-04-29 03:38:29 -0300

    feat: postgres on docker configured

--- api/src/main/java/com/scheffel/api/infra/cors/CorsConfig.java
+++ api/src/main/java/com/scheffel/api/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5174")
+                .allowedOrigins("http://localhost:5174", "http://localhost:5173") // Permite o acesso apenas de origens específicas
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS")
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true);

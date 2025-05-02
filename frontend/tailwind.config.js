4d0b995ad7f658ebc80cc6e3fa071efab8f44db6
muriloscheffel <murilo.scheffel@outlook.com>
2025-05-02 15:33:14 -0300

    setup tailwindcss

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: /home/scheffel/reps/plugdj/frontend/tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

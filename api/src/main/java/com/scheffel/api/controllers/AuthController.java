305a0734023aa042aac9a8e4c3a05d013334722a
muriloscheffel <murilo.scheffel@outlook.com>
2025-05-02 15:23:18 -0300

    fix: gitignore update and remove unused imports

--- api/src/main/java/com/scheffel/api/controllers/AuthController.java
+++ api/src/main/java/com/scheffel/api/controllers/AuthController.java
@@ -1,10 +1,8 @@
 package com.scheffel.api.controllers;
 
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.scheffel.api.dtos.LoginRequestDto;
 import com.scheffel.api.dtos.RegisterRequestDto;
 import com.scheffel.api.dtos.ResponseDto;
-import com.scheffel.api.infra.security.TokenService;
-import com.scheffel.api.models.User;
-import com.scheffel.api.repositories.UserRepository;
 import com.scheffel.api.services.AuthService;
 
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/auth")
